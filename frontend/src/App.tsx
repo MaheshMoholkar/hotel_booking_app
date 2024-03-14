47e98cd26d51b60cb581c2924246309a8e32dd0c
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-03-14 23:13:07 +0530

    add my-hotel component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,11 @@ import {
 import Layout from "./layouts/Layout";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
 
 function App() {
+  const { isLoggedIn } = useAppContext();
   return (
     <Router>
       <Routes>
@@ -43,7 +46,19 @@ function App() {
               <SignIn />
             </Layout>
           }
-        ></Route>
+        />
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+          </>
+        )}
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
