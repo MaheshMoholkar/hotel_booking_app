47fb15de1ad08f199b2f9123d266d6c759246ffb
Mahesh Moholkar <moholkarmahesh@aca.edu.in>
2024-03-29 16:27:32 +0530

    url fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,7 +33,7 @@ app.use('/api/users', userRoutes);
 app.use("/api/my-hotels", myHotelRoutes);
 
 app.get("*", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../../dist/index.html"))
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
 })
 
 app.listen(7000, () => {
